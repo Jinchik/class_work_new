271d19744e7d6f387c65055518273b58778c45fe
Test1 <Test1>
2022-01-29 14:27:45 +0200

    Lesson 11 and Tst

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-WTF IS GOING None
+word_without_vowels = ""
+user_word = str(input("Enter your word please "))
+user_word = user_word.upper()
+
+for i in user_word:
+   if i == "A" or i == "E" or i == "I" or i == "O" or i == "U": 
+       word_without_vowels = word_without_vowels + i
+       continue
+   print(i)
+print(word_without_vowels)
